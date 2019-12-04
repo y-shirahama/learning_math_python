a31a56b3e81aba4d08fcf42e8e25bf1d6148366d
y_shirahama <yuuya-shirahama@baby-calendar.jp>
2019-12-04 20:47:19 +0900

    実データに対して散布図を表示するようにした

--- database_operations.py
+++ database_operations.py
@@ -1,16 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # sample.csvの読み込み
 # df: data frame
-df = pd.read_csv('./tmp/sample.csv')
-# print(df)
+df = pd.read_csv('tmp/sample.csv')
+# print(df.head(5))
 
-# 先頭レコードから指定のレコード数を表示
-print(df.head(5))
-
-# データの抽出
+# 横軸をx,縦軸をyの散布図(scatter)をプロット
 x = df['x']
 y = df['y']
+plt.scatter(x, y)
+plt.show()
+print(df.mean())
 
-print(x)
-print(y)
+# データを中心化
+df_centering = df - df.mean()
+print(df_centering.mean())
+
+x_c = df_centering['x']
+y_c = df_centering['y']
+plt.scatter(x_c, y_c)
+plt.show()
