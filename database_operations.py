bcd245f3edb066d9f0fc8660cf425c589b9d2f21
y_shirahama <yuuya-shirahama@baby-calendar.jp>
2019-12-04 21:08:44 +0900

    単回帰分析における傾きaをプロットさせた

--- database_operations.py
+++ database_operations.py
@@ -11,13 +11,22 @@ x = df['x']
 y = df['y']
 plt.scatter(x, y)
 plt.show()
-print(df.mean())
+# print(df.mean())
 
 # データを中心化
 df_centering = df - df.mean()
-print(df_centering.mean())
+# print(df_centering.mean())
 
-x_c = df_centering['x']
-y_c = df_centering['y']
-plt.scatter(x_c, y_c)
+x = df_centering['x']
+y = df_centering['y']
+plt.scatter(x, y)
+plt.show()
+
+# 傾きaの計算
+xx = x * x
+xy = x * y
+a = xy.sum() / xx.sum()
+plt.scatter(x, y, label='y')                            # 実測値
+plt.plot(x, a*x, label='y_hat', color='red')            # 予測値
+plt.legend()
 plt.show()
