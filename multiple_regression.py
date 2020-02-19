4aa2dc2f6e13f7410f0883c5cbded52f0a0bb041
y_shirahama <yuuya-shirahama@baby-calendar.jp>
2020-02-19 22:20:09 +0900

    重回帰分析の練習

--- multiple_regression.py
+++ multiple_regression.py
@@ -10,24 +10,81 @@ import numpy as np
 '''
 
 # ベクトル定義
-x = np.array([1, 2, 3])
-print(x)
-print(type(x))
+x = np.array([[1], [2], [3]])
+# print(x)
+# print(type(x))
 
 # 行列の定義
-X = np.array([[1, 2], [3, 4]])
-print(X)
-print(type(X))
+X = np.array([
+    [1, 2], 
+    [3, 4]
+])
+# print(X)
+# print(type(X))
 
 # 転置
 Xt = X.T
-print(Xt)
+# print(Xt)
 
 # 逆行列
 # linear algebra:線形代数
 X_inv = np.linalg.inv(X)
-print(X_inv)
+# print(X_inv)
 
 # 行列積
 XX_inv = np.dot(X, X_inv)
-print(XX_inv)
+# print(XX_inv)
+
+'''
+下記の列ベクトルx、列ベクトルyの時、重みwを求めよ。
+x = [
+    [1, 2, 3],
+    [1, 2, 5],
+    [1, 3, 4],
+    [1, 5, 9]
+]
+y = [
+    [1], 
+    [5], 
+    [6], 
+    [8]
+]
+'''
+
+# Xの定義
+X = np.array([
+    [1, 2, 3],
+    [1, 2, 5],
+    [1, 3, 4],
+    [1, 5, 9]
+])
+
+# yの定義
+y = np.array([
+    [1],
+    [5],
+    [6],
+    [8]
+])
+
+# Step1:X転置行列を取得
+XtX = np.dot(X.T, X)
+print(XtX)
+
+# Step2:X転置行列の逆行列を取得
+Xtx_inv = np.linalg.inv(XtX)
+print(Xtx_inv)
+
+# Step3:y転置行列を取得
+Xty = np.dot(X.T, y)
+print(Xty)
+
+# Step4:重みwを求める
+w = np.dot(Xtx_inv, Xty)
+print(w)
+
+'''
+[[-0.14285714]
+ [ 0.71428571]
+ [ 0.57142857]]
+'''
